c436aa030b2136496fe86f37103beb9f30a36143
Tomasz <tomasz-korecki@wp.pl>
2016-03-21 09:06:59 +0100

    Working on grid boxes layout in the home page

--- ConnectYourself/connectYourselfWeb/app/controllers/homeController.js
+++ ConnectYourself/connectYourselfWeb/app/controllers/homeController.js
@@ -1,4 +1,13 @@
 ﻿'use strict';
 app.controller('homeController', ['$scope', 'authService', function ($scope, authService) {
 	$scope.authentication = authService.authentication;
+
+	$scope.newDeviceData = {
+		name: "",
+		cacheData: ""
+	};
+
+	$scope.onAddDevice = function() {
+		console.log($scope.newDeviceData);
+	}
 }]);
